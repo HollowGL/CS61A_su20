13cba03841d4304d63354fe87cc4b0f2091d1b22
hollowgl_mac <2363613192@qq.com>
2023-03-05 22:09:39 +0800

    hard

--- exam/61a-su20-mt/q7/q7.py
+++ exam/61a-su20-mt/q7/q7.py
@@ -50,24 +50,19 @@ def village(apple, t):
           31
           32
     """
-    # def graft(t, bs):
-    #     """
-    #     Grafts the given branches `bs` onto each leaf
-    #     of the given tree `t`, returning a new tree.
-    #     """
-    #     if ______:
-    #         return ______
-    #     new_branches = ______
-    #     return tree(______, ______)
-    # base_t = ______
-    # bs = ______
-    # return graft(base_t, bs)
-
-    def apple_helper(apple, t):
+    def graft(t, bs):
+        """
+        Grafts the given branches `bs` onto each leaf
+        of the given tree `t`, returning a new tree.
+        """
         if is_leaf(t):
-            return apple(label(t))
-        return tree(label(t), [apple_helper(apple, b) for b in branches(t)])
-    return apple_helper(apple, t)
+            return tree(label(t), bs)
+        new_branches = [graft(b, bs) for b in branches(t)]
+        return tree(label(t), new_branches)
+    base_t = apple(label(t))
+    bs = [village(apple, b) for b in branches(t)]
+    return graft(base_t, bs)
+
 
 def tree(label, branches=[]):
     """Construct a tree with the given label value and a list of branches."""
